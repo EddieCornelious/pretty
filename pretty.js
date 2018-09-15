793936d2c84285ca8579e89c6ec9925060e04842
Eddie Cornelious <eulerissmart@gmail.com>
2018-09-15 18:20:09 +0000

    fixed small bug

--- pretty.js
+++ pretty.js
@@ -13,10 +13,10 @@ var Pretty =
       return a % b;
     }
 
-    function formatOutput(w, colors, sizes) {
+    function formatOutput(message, colors, sizes) {
       var res = '';
       var css = [];
-      var word = w.toString().substring(0, 250);
+      var word = message.toString().substring(0, 250);
       for (var i = 0; i < word.length; i += 1) {
         var chosenColorIndex = mod(i, colors.length);
         var chosenSizeIndex = mod(i, sizes.length);
@@ -41,13 +41,13 @@ var Pretty =
     var defaultText = 'You forgot to enter a string to print!!';
     var defaultSize = ['25px'];
 
-    function log(str, colorArray, sizeArray) {
-      var word = str === undefined ? defaultText : word;
+    function log(message, colorArray, sizeArray) {
+      var word = message === undefined ? defaultText : message;
       var colors = colorArray === undefined ? rainbow : colorArray;
       var sizes = sizeArray === undefined ? defaultSize : sizeArray;
       var format = formatOutput(word, colors, sizes);
       console.log.apply(console, format);
       return this;
     }
-    return { log };
+    return {log};
   })();
